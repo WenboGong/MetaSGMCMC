440e5e98d8d78f47b68acdb761faafd9245c538b
WenboGong <GloveV659248@hotmail.com>
2018-06-18 11:04:43 +0100

    Update Sampler_Training_DataGen.py

--- BNN_MNIST/Sampler_Training_DataGen.py
+++ BNN_MNIST/Sampler_Training_DataGen.py
@@ -131,5 +131,5 @@ for ep in range(epoch):
         Adam_Q.step()
         Adam_D.step()
     if (ep+1)%10==0:
-        torch.save(Q_MLP.state_dict(), './tmp_model_save/Q_state_batch_500_baseline_50D_70G_step_0.007_100ep_broad_0.2_ep%s'%(ep+1))
-        torch.save(D_MLP.state_dict(), './tmp_model_save/D_state_batch_500_baseline_50D_70G_step_0.007_100ep_broad_0.2_ep%s'%(ep+1))
+        torch.save(Q_MLP.state_dict(), './tmp_model_save/Q_state_batch_500_baseline_50D_70G_step_0.007_100ep_broad_0.2_ep%s_DataGen'%(ep+1))
+        torch.save(D_MLP.state_dict(), './tmp_model_save/D_state_batch_500_baseline_50D_70G_step_0.007_100ep_broad_0.2_ep%s_DataGen'%(ep+1))
